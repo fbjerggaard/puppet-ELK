8aa65885171ee8681775ccbe1703a523252eb580
Frederik Bjerggaard Nielsen <mail@prooi.dk>
2016-02-17 23:44:27 +0100

    Update to latest versions plus minor changes

--- manifests/debian.pp
+++ manifests/debian.pp
@@ -1,54 +1,62 @@
 class elk::debian (
-  $es_ip = $elk::params::es_ip,
-  $es_cluster = $elk::params::es_cluster,
-  $es_node = $elk::params::es_node,
-  $es_package_url = $elk::params::es_package_url,
-  $ls_package_url = $elk::params::ls_package_url,
+  $es_ip              = $elk::params::es_ip,
+  $es_cluster         = $elk::params::es_cluster,
+  $es_node            = $elk::params::es_node,
+  $es_package_url     = $elk::params::es_package_url,
+  $ls_package_url     = $elk::params::ls_package_url,
   $kibana_package_url = $elk::params::kibana_package_url,
-  $es_config_dir = $elk::params::es_config_dir,
-  $listen_ip = $elk::params::listen_ip,
-) inherits elk::params {
-
-  if $::hardwaremodel == 'x86_64'{
-    #set exec path so sysctl works
-    Exec {path => '/usr/bin:/usr/sbin:/sbin:/bin'}
+  $es_config_dir      = $elk::params::es_config_dir,
+  $listen_ip          = $elk::params::listen_ip,) inherits elk::params {
+  if $::hardwaremodel == 'x86_64' {
+    # set exec path so sysctl works
+    Exec {
+      path => '/usr/bin:/usr/sbin:/sbin:/bin' }
 
     # Disable ipv6
-    sysctl::value { "net.ipv6.conf.all.disable_ipv6": value => "1" }->
-    sysctl::value { "net.ipv6.conf.default.disable_ipv6": value => "1" }->
-    sysctl::value { "net.ipv6.conf.lo.disable_ipv6": value => "1" }->
-
+    sysctl::value { "net.ipv6.conf.all.disable_ipv6": value => "1" } ->
+    sysctl::value { "net.ipv6.conf.default.disable_ipv6": value => "1" } ->
+    sysctl::value { "net.ipv6.conf.lo.disable_ipv6": value => "1" } ->
     anchor { 'elk::debian::begin': }
+
     class { 'elasticsearch':
       package_url  => $es_package_url,
       java_install => true,
     }
-    elasticsearch::instance { $es_node:
-      configdir => $es_config_dir
-    }
+
+    elasticsearch::instance { $es_node: configdir => $es_config_dir }
+
     anchor { 'elk::debian::end': }
+
     # logstash install
     class { 'logstash':
       package_url => $ls_package_url,
     }
+
     # kibana 4 install
     class { '::kibana4':
-      package_download_url => $kibana_package_url,
-      package_ensure       => '4.0.1-linux-x64',
-      package_provider     => 'archive',
-      manage_user          => true,
-      kibana4_user         => kibana4,
-      kibana4_group        => kibana4,
-      kibana4_gid          => 200,
-      kibana4_uid          => 200,
-  }
-  file { "/etc/logstash/conf.d/logstash.conf" :
-    ensure      => present,
-    content => template('elk/logstash.conf.erb'),
-  }
-  Anchor['elk::debian::begin'] -> Class['kibana4'] -> Class['logstash'] -> Anchor['elk::debian::end']
-  }
-  else {
-    fail("The ELK Module is only supported on 64 OS's")
+      package_provider  => 'package',
+      package_name      => 'kibana',
+      package_ensure    => '4.4.1',
+      manage_user       => false,
+      manage_init_file  => false,
+      service_name      => 'kibana',
+      kibana4_user      => 'kibana',
+      kibana4_group     => 'kibana',
+      use_official_repo => true,
+      repo_version      => '4.4',
+      config            => {
+        'port'              => 5601,
+        'host'              => '0.0.0.0',
+        'elasticsearch_url' => 'http://localhost:9200',
+      }
+    }
+
+    file { "/etc/logstash/conf.d/logstash.conf":
+      ensure  => present,
+      content => template('elk/logstash.conf.erb'),
+    }
+    Anchor['elk::debian::begin'] -> Class['kibana4'] -> Class['logstash'] -> Anchor['elk::debian::end']
+  } else {
+    fail("The ELK Module is only supported on 64bit operating systems")
   }
 }
--- manifests/params.pp
+++ manifests/params.pp
@@ -1,10 +1,10 @@
 class elk::params {
-  $es_ip = 'localhost'
-  $es_cluster = 'testcluster'
-  $es_node = 'testNode'
-  $es_package_url = 'https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-1.4.4.deb'
-  $ls_package_url = 'https://download.elasticsearch.org/logstash/logstash/packages/debian/logstash_1.4.2-1-2c0f5a1_all.deb'
-  $kibana_package_url = 'https://download.elasticsearch.org/kibana/kibana/kibana-4.0.1-linux-x64.tar.gz'
-  $es_config_dir = '/etc/elasticsearch/simspace'
-  $listen_ip = '10.10.10.10'
+  $es_ip              = 'localhost'
+  $es_cluster         = 'testcluster'
+  $es_node            = 'testNode'
+  $es_package_url     = 'https://download.elasticsearch.org/elasticsearch/release/org/elasticsearch/distribution/deb/elasticsearch/2.2.0/elasticsearch-2.2.0.deb'
+  $ls_package_url     = 'https://download.elastic.co/logstash/logstash/packages/debian/logstash_2.2.1-1_all.deb'
+  $kibana_package_url = 'https://download.elastic.co/kibana/kibana/kibana-4.4.1-linux-x64.tar.gz'
+  $es_config_dir      = '/etc/elasticsearch'
+  $listen_ip          = $::ipaddress
 }
